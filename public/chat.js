b1c4187e29373d3f9dd7edf502095d3dbc94db28
Muhammet Hadi KAMAT <muhammedhhadikamat@gmail.com>
2021-04-24 22:01:25 +0300

    edit

--- public/chat.js
+++ public/chat.js
@@ -1,25 +1,24 @@
 const socket = io.connect('http://localhost:3000');
-
 const message = document.getElementById('message');
 const submitBtn = document.getElementById('submitBtn');
 const output = document.getElementById('output');
 const feedback = document.getElementById('feedback');
-
+const userName = prompt("What is your name");
 submitBtn.addEventListener('click', ()=>{
     socket.emit('chat',{
         message:message.value,
+        name:userName
         
     });
-    console.log("Yazdi");
 });
 
 socket.on("chat", data=>{
     feedback.innerHTML="";
-    output.innerHTML+="<p><strong>"+"Muhamet"+" :</strong>"+data.message+"</p>"
+    output.innerHTML+="<p><strong>"+data.name+" :</strong>"+data.message+"</p>"
     message.value="";
 });
 message.addEventListener("keypress",()=>{
-    socket.emit("typing","Muhammet");
+    socket.emit("typing",userName);
 });
 socket.on("typing",data =>{
     feedback.innerHTML="<p>"+data+" yaziyor... </p>"
